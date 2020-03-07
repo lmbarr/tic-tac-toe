ea41f5a06081c6a26fac5aa96a41bfac6e1b3d95
Luis M <lmbrmiguelito@hotmail.com>
2020-03-07 18:04:33 -0500

    Play with templates

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User  # default model
-
+from django.db.models import Q
 
 GAME_STATUS_CHOICES = (
     ('F','First Player To Move'),
@@ -11,12 +11,26 @@ GAME_STATUS_CHOICES = (
 )
 
 
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+        return self.filter(
+            Q(first_player=user) | Q(second_player=user)
+        )
+
+    def active(self):
+        return self.filter(
+            Q(status='F') | Q(status='S')
+        )
+
+
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="games_first_player", on_delete=models.CASCADE)
     second_player = models.ForeignKey(User, related_name="games_second_player", on_delete=models.CASCADE)
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+    objects = GamesQuerySet.as_manager()
 
     def __str__(self):
         return f"{self.first_player} vs {self.second_player}"
