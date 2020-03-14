ee5b1ee16cf30a7a12cb609668fdd5bde42a793e
Luis M <lmbrmiguelito@hotmail.com>
2020-03-14 13:31:40 -0500

    Add login page

--- tictactoe/player/urls.py
+++ tictactoe/player/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .views import home
 
 urlpatterns = [
-    path('home/', home, name='player_home')
+    path('home/', home, name='player_home'),
+    path('login/', LoginView.as_view(template_name='player/login_form.html'), name='player_login'),
+    path('logout/', LogoutView.as_view(), name='player_logout')
 ]
