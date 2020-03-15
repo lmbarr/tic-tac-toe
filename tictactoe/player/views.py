6a0baf0dea3d49b4e0772c87118d6d939e2c0053
Luis M <lmbrmiguelito@hotmail.com>
2020-03-15 11:38:22 -0500

    Answer to an invitation to play

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,16 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect, get_object_or_404
 from gameplay.models import Game
 # Create your views here.
 from .models import Invitation
 from .forms import InvitationForm
 
+
 @login_required
 def home(request):
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()
-
-    return render(request, "player/home.html", {"games": active_games})
+    invitations = request.user.invitations_received.all()
+    return render(request, "player/home.html", {"games": active_games, "invitations": invitations})
 
 
 @login_required
@@ -25,3 +27,23 @@ def new_invitation(request):
         form = InvitationForm()
 
     return render (request, 'player/new_invitation_form.html', {'form': form})
+
+
+@login_required
+def accept_invitation(request, id):
+    invitation = get_object_or_404(Invitation, pk=id)
+    print(id)
+    print(request.method)
+
+    if not request.user == invitation.to_user:
+        raise PermissionDenied
+    if request.method == 'POST':
+        if 'accept' in request.POST:
+            game = Game.objects.create(first_player=invitation.to_user,
+                                       second_player=invitation.from_user)
+            invitation.delete()
+            return redirect('player_home')
+    else:
+        return render(request,
+                      'player/accept_invitation_form.html',
+                      {'invitation': invitation})
