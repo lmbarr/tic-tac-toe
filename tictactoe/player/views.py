4395dc0e70050020a4f6fcf4fa1d3d8f1b204dd4
Luis M <lmbrmiguelito@hotmail.com>
2020-03-14 19:04:05 -0500

    Add invitation form

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from gameplay.models import Game
 # Create your views here.
+from .models import Invitation
+from .forms import InvitationForm
 
 @login_required
 def home(request):
@@ -9,3 +11,17 @@ def home(request):
     active_games = my_games.active()
 
     return render(request, "player/home.html", {"games": active_games})
+
+
+@login_required
+def new_invitation(request):
+    if request.method == 'POST':
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invitation, data=request.POST)
+        if form.is_valid():
+            form.save() # save row in DB
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
+
+    return render (request, 'player/new_invitation_form.html', {'form': form})
